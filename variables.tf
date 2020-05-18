04a8b67cb81f3f7e27141fd158a7e122655fb3eb
pooja24n <60745414+pooja24n@users.noreply.github.com>
2020-05-18 15:46:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = "string"
+    default = "AKIAQ5KPARMMWA3IRJFJ"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "YU6dwkLbwVBeQAiHqFbQS4BLwF6t1zYDiQr34QLU"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-east-2"
+}
+
+variable "instance_type" {
+    type = "string"
+    default = ""
+}
